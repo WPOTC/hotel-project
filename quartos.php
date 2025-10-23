4034e13fd6a3df7c493bca6b5b1d6a66f7b3b1ca
VitorSanto-ux <vitor.h.santo@alunosenai.br>
2025-10-23 10:18:18 -0300

    criacao dos quartos

---
+++ quartos.php
@@ -0,0 +1,61 @@
+<!DOCTYPE html>
+<html lang="pt-br">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Document</title>
+</head>
+<body>
+    <h1>Quartos</h1>
+    
+    <h2>Suíte Presidencial</h2>
+    <h3>R$ 1.187,68</h3>
+
+    <button type="submit">Agendar</button>
+
+    <h2>Suíte Master</h2>
+    <h3>R$ 559,90</h3>
+
+    <button type="submit">Agendar</button>
+
+    <h2>Suíte Premium / Deluxe</h2>
+    <h3>R$ 570,00</h3>
+
+    <button type="submit">Agendar</button>
+
+    <h2>Suíte Júnior</h2>
+    <h3>R$ 254,00</h3>
+
+    <button type="submit">Agendar</button>
+
+    <h2>Spa Suite</h2>
+    <h3>R$ 999,00</h3>
+
+    <button type="submit">Agendar</button>
+
+    <h2>Suíte econômica</h2>
+    <h3>R$ 400,00</h3>
+
+    <button type="submit">Agendar</button>
+
+    <h2>Suíte Real</h2>
+    <h3>R$ 390,00</h3>
+
+    <button type="submit">Agendar</button>
+
+    <h2>Casa de campo</h2>
+    <h3>R$ 1.500,00</h3>
+
+    <button type="submit">Agendar</button>
+
+    <h2>Suíte elegante</h2>
+    <h3>R$ 135,00</h3>
+
+    <button type="submit">Agendar</button>
+
+    <h2>Suíte mais mais</h2>
+    <h3>R$ 24.000,00</h3>
+
+    <button type="submit">Agendar</button>
+</body>
+</html>
