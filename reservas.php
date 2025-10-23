1cbd4b481006bc71d7a902b202314d43000eca74
sophboo <boonobody0@gmail.com>
2025-10-23 13:58:40 -0300

    reservas finalizadas

---
+++ reservas.php
@@ -0,0 +1,38 @@
+<!DOCTYPE html>
+<html lang="pt-br">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Document</title>
+</head>
+<body>
+     <nav>
+        <img src="" alt="">
+
+        <h1>Hotel Villa do Sol</h1>
+
+        <ul>
+    <li><a href="index.php">INICIO</a></li>
+    <li><a href="quartos.php">QUARTOS</a></li>
+    <li><a href="sobre.php">SOBRE NÓS</a></li>
+</ul>
+    </nav>
+
+    <div>
+        <h1>Faça sua reserva</h1>
+        <img src="" alt="">
+
+          <form>
+        Quantos hospedes viream: <input type="number" name="Quantidade" min="1" max="4" step="2"><br><br>
+
+        Data de entrada: <input type="date" name="Data de entrada"><br><br>
+
+        Data de saída: <input type="date" name="Data de saída"><br><br>
+
+        <input type="submit" value="Reservar">  
+
+
+    </form>
+    </div>
+</body>
+</html>
