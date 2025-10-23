77829456569dbf001ae4231663d45326329620fe
Ana Luisa Anselmo <analuisaanselmo2008@gmail.com>
2025-10-23 11:22:14 -0300

    criaçao css

--- index.php
+++ index.php
@@ -1,48 +1,57 @@
 <!DOCTYPE html>
 <html lang="en">
-<head>
-    <meta charset="UTF-8">
-    <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <title>Hotel Villa do Sol</title>
-</head>
-<body>
-    <nav>
-        <img src="" alt="">
 
+<head>
+  <meta charset="UTF-8">
+  <meta name="viewport" content="width=device-width, initial-scale=1.0">
+  <title>Hotel Villa do Sol</title>
+  <link rel="stylesheet" href="css/index.css">
+</head>
+
+<body>
+  <nav>
+    <div class="menu">
+      <img src="img/logo-2.png" alt="">
+
+      <div class="textos-nav">
         <h1>Hotel Villa do Sol</h1>
 
         <ul>
-    <li><a href="index.php">INICIO</a></li>
-    <li><a href="quartos.php">QUARTOS</a></li>
-    <li><a href="sobre.php">SOBRE NÓS</a></li>
-</ul>
-    </nav>
+          <li><a href="index.php">INICIO</a></li>
+          <li><a href="quartos.php">QUARTOS</a></li>
+          <li><a href="sobre.php">SOBRE NÓS</a></li>
 
-    <div>
-        <img src="" alt="">
-        <p>Respire fundo.
-        Ouça o som dos pássaros e sinta o toque da brisa suave entre as árvores.
-        Aqui, o tempo desacelera e o luxo se revela em cada detalhe.
-        Villa do Sol – o seu refúgio natural de elegância e paz.
-        </p>
-
-        <p>Villa do Sol – Onde o luxo encontra a natureza.
-        Descubra um refúgio exclusivo entre o verde e o dourado do pôr do sol.
-        Chalés de madeira, conforto cinco estrelas e uma experiência que brilha com a energia do paraíso.
-        Villa do Sol — viva o luxo em sua forma mais natural.
-        </p>
-
-        <img src="" alt="">
-
+        </ul>
+      </div>
     </div>
+  </nav>
 
-    <footer class="footer-simple" role="contentinfo">
-  <div class="container">
-    <p>© 2025 Hotel Villa do Sol . Todos os direitos reservados.
-        Número de contato: (11) 1234-5678. Email:villasol@gmail.com 
+  <div>
+    <img src="" alt="">
+    <p>Respire fundo.
+      Ouça o som dos pássaros e sinta o toque da brisa suave entre as árvores.
+      Aqui, o tempo desacelera e o luxo se revela em cada detalhe.
+      Villa do Sol – o seu refúgio natural de elegância e paz.
     </p>
+
+    <p>Villa do Sol – Onde o luxo encontra a natureza.
+      Descubra um refúgio exclusivo entre o verde e o dourado do pôr do sol.
+      Chalés de madeira, conforto cinco estrelas e uma experiência que brilha com a energia do paraíso.
+      Villa do Sol — viva o luxo em sua forma mais natural.
+    </p>
+
+    <img src="" alt="">
+
   </div>
-</footer>
+
+  <footer class="footer-simple" role="contentinfo">
+    <div class="container">
+      <p>© 2025 Hotel Villa do Sol . Todos os direitos reservados.
+        Número de contato: (11) 1234-5678. Email:villasol@gmail.com
+      </p>
+    </div>
+  </footer>
 
 </body>
+
 </html>
