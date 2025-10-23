943b62c75560bdc410385d29b28b07144b30f8ad
VitorSanto-ux <vitor.h.santo@alunosenai.br>
2025-10-23 08:47:24 -0300

    continuacao

--- quartos/quarto10.php
+++ quartos/quarto10.php
@@ -1,3 +1,16 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Document</title>
+</head>
+<body>
+    <button type="submit">Agendar</button>
+    <button type="submit">Cancelar</button>
+</body>
+</html>
+
 <?php
 
 $nomedohospede;
