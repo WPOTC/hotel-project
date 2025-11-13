239037b350694b471dfe2cb012ff17a818e4e318
Ana Luisa Anselmo <analuisaanselmo2008@gmail.com>
2025-11-13 08:45:06 -0300

    concertando quartos

--- View/quartos/ListarQuartos.php
+++ View/quartos/ListarQuartos.php
@@ -1,7 +1,10 @@
 <?php
 
-//require_once "C:/Turma1/xampp/htdocs/hotel-project/DB/Database.php";
-//require_once "C:/Turma1/xampp/htdocs/hotel-project/Controller/QuartosController.php";
+require_once "C:/Turma1/xampp/htdocs/hotel-project/DB/Database.php";
+require_once "C:/Turma1/xampp/htdocs/hotel-project/Controller/QuartosController.php";
+
+global $pdo;
+
 
 
 // Cria o controlador
@@ -25,7 +28,32 @@
 
 foreach ($Quartos as $quarto) {
   $id = $quarto['id'];
-  echo "<img style='height: 200px;' src='uploads/quartos/".$quarto['caminho_imagem'] . "'><br>";
+
+  echo "<div class='bloco1'>
+
+        <div class='slider'>
+
+            <div class='slides'>
+
+                <img src='../../uploads/quartos/{$quarto['caminho_imagem']}' class='active'>
+            </div>
+
+            <button class='prev'>⟨</button>
+            <button class='next'>⟩</button>
+            
+            <h2>Suíte Presidencial</h2>
+
+            <p>R$ 1.187,68</p>
+
+           <a href='quartos/quarto1.php'> <button type='submit' class='botao'>Agendar</button></a>
+
+        </div>
+
+    </div>";
+
+
+
+  //echo "<img style='height: 200px;' src='uploads/quartos/".$quarto['caminho_imagem'] . "'><br>";
   echo $quarto['nome_quarto'] . "<br";
   echo $quarto['valor'] . "<br>";
 }
