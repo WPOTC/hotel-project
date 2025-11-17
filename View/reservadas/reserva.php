1559b1e7c7d4e90e594055c6475f136281663101
Ana Luisa Anselmo <analuisaanselmo2008@gmail.com>
2025-11-17 10:46:36 -0300

    mais algumas mudanças

--- View/reservadas/reserva.php
+++ View/reservadas/reserva.php
@@ -34,8 +34,14 @@ session_start();
           <li><a href="View/reservadas/listarReserva.php">RESERVAS</a></li>
 
         </ul>
-      </div>
-    </div>';
+      </div>';
+       if (isset($_SESSION['nome'])) {
+    echo "<div class='usuario'><a href = '../../View/Usuario/exibirUsuario.php'class='cadastro'><img src='../../img/logo-cadastro-feito.png'></a>" . "Seja bem-vindo(a),  " . htmlspecialchars($_SESSION['nome']) . "!</div>";
+  } elseif (!isset($_SESSION['nome'])) {
+    echo "<div class='usuario'><a href = '../../View/Usuario/cadastrarUsuario.php' class='cadastro'><img src='../../img/logo-cadastro.png'></a></div>";
+
+  }
+  echo '</div>';
   } else {
     echo '<nav>
     <div class="menu">
@@ -56,70 +62,71 @@ session_start();
     </div>';
   }
 
-  if (isset($_SESSION['nome'])) {
-    echo "<div class='usuario'><a href = '../View/Usuario/exibirUsuario.php'class='cadastro'><img src='../../img/logo-cadastro-feito.png'></a>" . "Seja bem-vindo(a),  " . htmlspecialchars($_SESSION['nome']) . "!</div>";
+  if (isset($_SESSION['nome']) && $_SESSION['nome'] != 'Villa do Sol') {
+    echo "<div class='usuario'><a href = '../../View/Usuario/exibirUsuario.php'class='cadastro'><img src='../../img/logo-cadastro-feito.png'></a>" . "Seja bem-vindo(a),  " . htmlspecialchars($_SESSION['nome']) . "!</div>";
   } elseif (!isset($_SESSION['nome'])) {
-    echo "<a href = 'View/Usuario/cadastrarUsuario.php' class='cadastro'><img src='../../img/logo-cadastro.png'></a>";
+    echo "<div class='usuario'><a href = '../../View/Usuario/cadastrarUsuario.php' class='cadastro'><img src='../../img/logo-cadastro.png'></a></div>";
 
   }
   ?>
-</div>
+  </div>
   </nav>
-  
+
   <a href="../../quartos.php" class="cadastro-voltar"><img src="../../img/logo-voltar.png"></a>
-    
+
   <section>
     <header>
       <img src="../../img/logo.png" alt="">
       <h1 class="cortitulo">Faça sua reserva</h1><br>
     </header>
-        
-    
+
+
     <form method="POST">
 
-    <?php
-    // Conexão
-    $pdo = new PDO("mysql:host=localhost;dbname=hotel", "root", "");
+      <?php
+      // Conexão
+      $pdo = new PDO("mysql:host=localhost;dbname=hotel", "root", "");
 
-    // Busca dos hóspedes
-    $stmt = $pdo->query("SELECT id, nome, telefone FROM cadastro");
-    $hospedes = $stmt->fetchAll(PDO::FETCH_ASSOC);
+      // Busca dos hóspedes
+      $stmt = $pdo->query("SELECT id, nome, telefone FROM cadastro");
+      $hospedes = $stmt->fetchAll(PDO::FETCH_ASSOC);
 
-    // Busca dos quartos
-    $stmt2 = $pdo->query("SELECT id, nome_quarto FROM quartos");
-    $quartos = $stmt2->fetchAll(PDO::FETCH_ASSOC);
-    ?>
+      // Busca dos quartos
+      $stmt2 = $pdo->query("SELECT id, nome_quarto FROM quartos");
+      $quartos = $stmt2->fetchAll(PDO::FETCH_ASSOC);
+      ?>
 
-    <!-- DROPDOWN DE QUARTOS -->
-    <label for="id_quarto">Selecione o Quarto:</label>
-    <select name="id_quarto" id="id" class="form-control" required>
-        <option value="">-- Escolha um quarto --</option>
+      <!-- DROPDOWN DE QUARTOS -->
+      <label for="id_quarto">Selecione o Quarto:</label>
+      <select name="id_quarto" id="id" class="form-control" required>
+        <option value=""> - Escolha um quarto - </option>
         <?php foreach ($quartos as $q): ?>
-            <option value="<?= $q['id'] ?>">
-                <?= $q['id'] ?> - <?= $q['nome_quarto'] ?>
-            </option>
+          <option value="<?= $q['id'] ?>">
+            <?= $q['id'] ?> - <?= $q['nome_quarto'] ?>
+          </option>
         <?php endforeach; ?>
-    </select>
+      </select>
 
-    <br><br>
+      <br><br>
 
-  
 
-    <!-- DATA -->
-    <label>Data da reserva:</label>
-    <input type="date" name="data" required>
 
-    <br><br>
+      <!-- DATA -->
+      <label>Data da reserva:</label>
+      <input type="date" name="data" required>
 
-    <button type="submit">Reservar</button>
-</form> 
+      <br><br>
+
+      <button><a href='../View/reservadas/quartos.php'>Reservar</a></button>
+
+    </form>
   </section>
   <footer class="footer-simple" role="contentinfo">
     <div class="container">
       <p>© 2025 Hotel Villa do Sol . Todos os direitos reservados.
         Número de contato: (11) 1234-5678. Email:villasol@gmail.com
       </p>
-      <button><a href='../View/reservadas/reserva.php'>Agendar</a></button>
+
     </div>
   </footer>
   <?php
@@ -136,10 +143,10 @@ session_start();
 
     $controller = new ReservasController($pdo);
 
-    if(!isset($_SESSION['nome'])){
-            echo "Você precisa estar logado na sua conta antes de reservar.";
+    if (!isset($_SESSION['nome'])) {
+      echo "Você precisa estar logado na sua conta antes de reservar.";
 
-        }elseif(isset($_SESSION['nome'])) { 
+    } elseif (isset($_SESSION['nome'])) {
       $controller->reservar($reserva, $idQuarto, $idUsuario);
       echo "Reserva feita com sucesso!";
     } else {
